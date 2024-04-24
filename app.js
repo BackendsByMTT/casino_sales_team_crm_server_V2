957ce95dbe0b5b4e2a9f4f14836206d021062a6c
Anushaka23 <anushaka.underpinservices@gmail.com>
2024-04-24 17:37:54 +0530

    fix changes

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const cookieParser = require('cookie-parser');
-const routes = require("./routes")
+const routes = require("./src/routes")
 const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 require("dotenv").config()
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors());    // add domain
 
 app.use("/", routes)
 
